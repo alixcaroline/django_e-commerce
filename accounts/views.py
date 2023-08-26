1393f8724ef1c96e2d3af2881693033fd4897664
alixcaroline <alixcaroline13@gmail.com>
2023-08-26 11:50:07 +0200

    change password functionality and view order details funcionality

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ from django.core.mail import EmailMessage
 
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
-from orders.models import Order
+from orders.models import Order, OrderProduct
 import requests
 
 
@@ -165,7 +165,12 @@ def dashboard(request):
         user_id=request.user.id, is_ordered=True
     )
     orders_count = orders.count()
-    context = {"orders_count": orders_count}
+
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {
+        "orders_count": orders_count,
+        "userprofile": userprofile,
+    }
     return render(request, "accounts/dashboard.html", context)
 
 
@@ -265,3 +270,45 @@ def edit_profile(request):
         "userprofile": userprofile,
     }
     return render(request, "accounts/edit_profile.html", context)
+
+
+@login_required(login_url="login")
+def change_password(request):
+    if request.method == "POST":
+        current_password = request.POST["current_password"]
+        new_password = request.POST["new_password"]
+        confirm_password = request.POST["confirm_password"]
+
+        user = Account.objects.get(username__exact=request.user.username)
+
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request)
+                messages.success(request, "Password updated successfully.")
+                return redirect("change_password")
+            else:
+                messages.error(request, "Please enter valid current password")
+                return redirect("change_password")
+        else:
+            messages.error(request, "Password does not match!")
+            return redirect("change_password")
+    return render(request, "accounts/change_password.html")
+
+
+@login_required(login_url="login")
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+
+    context = {
+        "order_detail": order_detail,
+        "order": order,
+        "subtotal": subtotal,
+    }
+    return render(request, "accounts/order_detail.html", context)
